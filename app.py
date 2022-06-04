e176e41846561cb822aea65eeab12e88be0e8860
Chetan <chetanatoffice@gmail.com>
2022-06-04 21:57:14 +0530

    second

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from recom import search , search_by_keyword
+from recom import search , search_by_keyword , listt
 from flask import Flask,render_template ,request
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ def index():
 @app.route("/result" , methods=["POST","GET"])
 def res():
     if request.method =="POST":
-        title,genre,rating,releas,pr,nr,nur = search(request.form["movie_name"], int(request.form["numbers"]),int(request.form["rate"]))
-        x = zip(title,genre,rating,releas,pr,nr,nur)
+        title,genre,rating,releas,pr,nr,nur,img = search(request.form["movie_name"], int(request.form["numbers"]),int(request.form["rate"]))
+        x = zip(title,genre,rating,releas,pr,nr,nur,img)
         return render_template("result.html", x = x)
 
     return render_template("Flim.html")
@@ -20,10 +20,18 @@ def res():
 @app.route("/keyword", methods=["POST","GET"])
 def key():
     if request.method =="POST":
-        title,genre,rating,releas,pr,nr,nur = search_by_keyword(request.form["keyw"], int(request.form["numbers"]), int(request.form["rate"]))
-        x = zip(title,genre,rating,releas,pr,nr,nur)
+        title,genre,rating,releas,pr,nr,nur,img = search_by_keyword(request.form["keyw"], int(request.form["numbers"]), int(request.form["rate"]))
+        x = zip(title,genre,rating,releas,pr,nr,nur,img)
         return render_template("keyresult.html", x = x)
     return render_template("keyword.html")
+
+
+@app.route("/list")
+def listtt():
+    title,genre,rating,releas,pr,nr,nur,img = listt()
+    x = zip(title,genre,rating,releas,pr,nr,nur,img)
+    return render_template("list.html",x = x)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- recom.py
+++ recom.py
@@ -5,27 +5,27 @@ import ast
 import warnings
 warnings.filterwarnings("ignore")
 vec = TfidfVectorizer()
-data = pd.read_csv("nlp with lower.csv")
+data = pd.read_csv("Nlp with images.csv")
 data.fillna("sorry",inplace=True)
 vector = vec.fit_transform(data["ggg"])
 indexed = pd.Series(data = data.index,index=data.lower)
 
 def search(n, top = 10,rate = 3):
     if n.lower() in indexed:
-        rec = pd.DataFrame(list(linear_kernel(vector[indexed[n]],vector)))
+        rec = pd.DataFrame(list(linear_kernel(vector[indexed[n.lower()]],vector)))
         rec = rec.transpose()
         rec.columns = ["name"]
         rec = rec.sort_values(by="name" ,ascending=False)
         x = pd.DataFrame()
         for i in range(0,min(top,800)):
             x = x.append(data.loc[rec.index[i]:rec.index[i]])
-        x = x[x["Ratinng"] > rate]
+        x = x[(x["Ratinng"] > rate) & (x["img"] != "sorry") ]
         x = x.sort_values(by="Ratinng",ascending=False)
         
 #         display(x)
     else:
         return ("Not Found")
-    return list(x["Movie Title"]), list(x.ggg), list(x.Ratinng),list(x["Release Date"]),list(x["Positive reviews"]),list(x["Negative reviews"]),list(x["Nuteral  reviews"])
+    return list(x["Movie Title"]), list(x.ggg), list(x.Ratinng),list(x["Release Date"]),list(x["Positive reviews"]),list(x["Negative reviews"]),list(x["Nuteral  reviews"]),list(x["img"])
 
 
 def search_by_keyword(n, top = 10,rate = 3):
@@ -41,6 +41,10 @@ def search_by_keyword(n, top = 10,rate = 3):
     for i in range(1,min(top,800)):
         x = x.append(data.loc[rec.index[i]:rec.index[i]])
     x = x[x["Ratinng"] > rate]
-    return list(x["Movie Title"]), list(x.ggg), list(x.Ratinng),list(x["Plot"]),list(x["Positive reviews"]),list(x["Negative reviews"]),list(x["Nuteral  reviews"])
+    return list(x["Movie Title"]), list(x.ggg), list(x.Ratinng),list(x["Plot"]),list(x["Positive reviews"]),list(x["Negative reviews"]),list(x["Nuteral  reviews"]),list(x["img"])
 
-
+def listt():
+    data = pd.read_csv("Nlp with images.csv")
+    data = data[ data["img"] != "sorry" ]
+    data = data.sort_values(by="Ratinng",ascending=False)
+    return list(data["Movie Title"]), list(data.ggg), list(data.Ratinng),list(data["Release Date"]),list(data["Positive reviews"]),list(data["Negative reviews"]),list(data["Nuteral  reviews"]),list(data["img"])
